eb2620a2f7e8c3d66b73cb130004fa036321839b
Muhammad Bilal <bilalommerr@gmail.com>
2026-02-12 16:35:32 -0600

    Coded the default constructor and the overload constructor and also Java Doc'd some of the code.

--- Account.java
+++ Account.java
@@ -1,5 +1,13 @@
 public class Account {
-    private static int lastId = 0; //
+
+    /**
+     * Class variable for maintaining the last account used.
+     */
+    private static int lastId = 0;
+
+    /**
+     * The unique account number used by the default account number.
+     */
 
     private final int id = 0;  //
 
@@ -11,10 +19,27 @@ public class Account {
 
 
 
+    public Account() {
 
+        this.id = ++lastId;
+        this.firstName = "";
+        this.lastName = "";
+        this.balance = 0.0;
+    }
 
+    /**
+     *The overload constructor calls the default constructor and then allows overriding the default values for the
+     * account owners name for calling the setter for data validation.
+     * @param firstName - Account Owners first name
+     * @param lastName  - Account Owners last name
+     */
 
+    public Account(String firstName, String lastName) {
 
+        this();
+        setFirstName(firstName);
+        setLastName(lastName);
 
+    }
 
 }
