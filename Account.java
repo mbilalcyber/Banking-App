70ee06048584c8cf5ba0e0fe4d0c56e7f23f60c5
Muhammad Bilal <bilalommerr@gmail.com>
2026-02-12 16:14:36 -0600

    Added Fields and initialized them

---
+++ Account.java
@@ -0,0 +1,20 @@
+public class Account {
+    private static int lastId = 0; //
+
+    private final int id = 0;  //
+
+    private String firstName;
+
+    private String lastName;
+
+    private double balance;
+
+
+
+
+
+
+
+
+
+}
